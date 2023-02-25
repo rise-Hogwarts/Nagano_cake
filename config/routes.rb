55ddf06906574129d3b3a1b4dfaa568d83c496f4
EC2 Default User <ec2-user@ip-172-31-2-119.ap-northeast-1.compute.internal>
2023-02-25 16:47:54 +0000

    [add]new-file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  devise_for :admin
   devise_for :customers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+
+  namespace :public do  #顧客・会員
+    root to: 'homes#top'
+    get '/about' => 'homes#about'
+  end
+
 end
