ac841f55b3428fbdfbc9d4e7094db4bfc43cf47e
EC2 Default User <ec2-user@ip-172-31-2-119.ap-northeast-1.compute.internal>
2023-02-27 08:07:18 +0000

    [add]file

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,34 @@
 Rails.application.routes.draw do
-  devise_for :admin
-  devise_for :customers
+  devise_for :customers,skip: [:passwords], controllers: {
+   registrations: "public/registrations",
+   sessions: 'public/sessions'
+  }
+
+  devise_for :admin,skip: [:registrations, :passwords], controllers: {
+   sessions: "admin/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 
 
-  namespace :public do  #顧客・会員
+  scope module: :public do#顧客・会員
     root to: 'homes#top'
     get '/about' => 'homes#about'
+    resources :items, only: [:index, :show]
+    resources :customers, only: [:show, :edit, :update, :confirm, :withdraw]
+    resources :cart_items, only: [:index, :update, :destroy, :destroy_all, :create]
+    resources :orders, only: [:new, :confirm, :complete, :create, :index, :show]
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+  end
+
+
+  namespace :admin do
+    get '/admin' => 'homes#top'
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :order_details, only: [:update]
+    resources :orders, only: [:show, :update]
   end
 
 end
