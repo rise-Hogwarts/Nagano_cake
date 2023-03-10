718f92861209303005df1d35a9783ac825a0286a
EC2 Default User <ec2-user@ip-172-31-2-119.ap-northeast-1.compute.internal>
2023-03-10 16:55:51 +0000

    [save]nagano_cake

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,9 +1,14 @@
 class Public::CartItemsController < ApplicationController
 
   def index
+    @cart_items = current_customer.cart_items
   end
 
   def update
+    cart_item = CartItem.find(params[:id])
+    cart_item.customer_id = current_customer.id
+    cart_item.update(cart_item_params)
+    redirect_to cart_items_path
   end
 
   def destroy
@@ -13,6 +18,15 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item.customer_id = current_customer.id
+    @cart_item.save
+    redirect_to cart_items_path
+  end
+
+  private
+  def cart_item_params
+      params.require(:cart_item).permit(:item_id, :amount)
   end
 
 end
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,4 +1,5 @@
 class Public::ItemsController < ApplicationController
+  before_action :authenticate_customer!
 
   def index
     @items = Item.page(params[:page])
@@ -6,5 +7,8 @@ class Public::ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+    @cart_item = CartItem.new
   end
+
+
 end
